2321b12a28adc77035f8e067a4c43511e3771c2a
Renan <renanestery1@gmail.com>
2025-01-22 17:56:38 -0300

    feat: add method update and index for tickets

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { knex } from "@/database/knex";
 import {z} from "zod"
+import { AppError } from "@/utils/AppError";
 
 class TicketsController{
 
@@ -10,7 +11,7 @@ class TicketsController{
                 equipment: z.string().min(3).max(15),
                 description: z.string().min(8).max(50),
                 user_name: z.string().min(3).max(15),
-                status: z.string().optional(),
+                status: z.string().default("open"),
             })
 
             const {equipment, description, user_name, status} = bodySchema.parse(request.body)
@@ -22,6 +23,53 @@ class TicketsController{
         } catch (error) {
             next(error)
         }
+    }
+
+    async index(request: Request, response: Response, next: NextFunction){
+        try {
+            const {status} = request.query
+            const tickets = await knex<TicketsRepository>("tickets")
+            .select()
+            .whereLike("status", `%${status ?? ""}%`)
+            return response.json(tickets)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async update(request: Request, response: Response, next: NextFunction){
+      try {
+        const id = z
+        .string().transform((value) => Number(value))
+        .refine((value) => !isNaN(value), {message: "id must be a number"})
+        .parse(request.params.id)
+
+        const bodySchema = z.object({
+            equipment: z.string().min(3).max(15),
+            description: z.string().min(8).max(50),
+        })
+
+        const {equipment, description} = bodySchema.parse(request.body)
+
+        const ticket = await knex<TicketsRepository>("tickets")
+        .select()
+        .where({id})
+        .first()
+
+        if(!ticket){
+            throw new AppError("product not found")
+        }
+
+        await knex<TicketsRepository>("tickets")
+        .update({equipment, description, updated_at: knex.fn.now()})
+        .where({id})
+
+
+        return response.json()
+      } catch (error) {
+        next(error)
+      }
+
     }
 }
 
--- src/routes/tickets-routes.ts
+++ src/routes/tickets-routes.ts
@@ -5,5 +5,7 @@ const ticketsRoutes = Router()
 const ticketsController = new TicketsController
 
 ticketsRoutes.post("/", ticketsController.create)
+ticketsRoutes.get("/", ticketsController.index)
+ticketsRoutes.put("/:id", ticketsController.update)
 
 export {ticketsRoutes}
